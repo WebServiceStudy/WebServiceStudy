d4f8752b66f7683b35b5f33f62d18d3c5d9dbfc5
hongcoding94 <towijin2100@gmail.com>
2022-11-15 01:25:58 +0900

    환경셋팅 ver 0.3. swagger 셋팅 추가 및 수정

--- src/main/java/com/wss/webservicestudy/web/common/swagger/config/SwaggerConfig.java
+++ src/main/java/com/wss/webservicestudy/web/common/swagger/config/SwaggerConfig.java
@@ -1,34 +1,106 @@
 package com.wss.webservicestudy.web.common.swagger.config;
 
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 @EnableSwagger2
+@SuppressWarnings("unchecked")
 public class SwaggerConfig {
 
+    // API마다 구분짓기 위한 설정.
     @Bean
-    public Docket restAPI() {
+    public Docket allApi() {
+        return restAPI("전체", Predicates.or(
+                PathSelectors.regex("/*.*")));
+    }
+
+    @Bean
+    public Docket commonApi() {
+        return restAPI("공통", Predicates.or(
+                PathSelectors.regex("/common.*")));
+    }
+
+    @Bean
+    public Docket productApi() {
+        return restAPI("유저", Predicates.or(
+                PathSelectors.regex("/user.*")));
+    }
+
+    @Bean
+    public Docket feedApi() {
+        return restAPI("게시판", Predicates.or(
+                PathSelectors.regex("/feed.*")));
+    }
+
+    @Bean
+    public Docket adminApi() {
+        return restAPI("관리페이지", Predicates.or(
+                PathSelectors.regex("/manager.*")));
+    }
+
+    @Bean
+    public Docket matcomApi() {
+        return restAPI("매칭", Predicates.or(
+                PathSelectors.regex("/matcom.*")));
+    }
+
+    @Bean
+    public Docket searchApi() {
+        return restAPI("배치", Predicates.or(
+                PathSelectors.regex("/reservation.*")));
+    }
+
+    public Docket restAPI(String groupName, Predicate<String> predicate) {
         return new Docket(DocumentationType.SWAGGER_2)
+                .groupName(groupName)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.wss.webservicestudy.web"))
-                .paths(PathSelectors.any())
-                .build();
+                .paths(predicate)
+                .apis(RequestHandlerSelectors.any())
+                .build()
+                .globalResponseMessage(RequestMethod.GET, getCustomizedResponseMessages())
+                .globalResponseMessage(RequestMethod.POST, getCustomizedResponseMessages())
+                .globalResponseMessage(RequestMethod.DELETE, getCustomizedResponseMessages())
+                .globalResponseMessage(RequestMethod.PUT, getCustomizedResponseMessages())
+                .globalResponseMessage(RequestMethod.PATCH, getCustomizedResponseMessages());
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("hobbybee Spring Boot REST API")
+                .title("WSS Spring Boot REST API")
                 .version("1.0.0")
-                .description("취미 생활 커뮤니티 hobbybee의 swagger api 입니다.")
+                .description("Web Service Study 프로젝트")
                 .build();
+    }
+
+    private List<ResponseMessage> getCustomizedResponseMessages() {
+        List<ResponseMessage> responseMessages = new ArrayList<>();
+        responseMessages.add(new ResponseMessageBuilder().code(200).message("성공").build());
+        responseMessages.add(new ResponseMessageBuilder().code(204).message("데이터 미존재").build());
+        responseMessages.add(new ResponseMessageBuilder().code(400).message("입력값 오류").build());
+        responseMessages.add(new ResponseMessageBuilder().code(401).message("미 로그인").build());
+        responseMessages.add(new ResponseMessageBuilder().code(403).message("권한없음").build());
+        responseMessages.add(new ResponseMessageBuilder().code(404).message("경로 오류").build());
+        responseMessages.add(new ResponseMessageBuilder().code(412).message("처리중 오류").build());
+        responseMessages.add(new ResponseMessageBuilder().code(500).message("서버에러").build());
+        return responseMessages;
     }
 }
--- src/main/java/com/wss/webservicestudy/web/user/controller/UserController.java
+++ src/main/java/com/wss/webservicestudy/web/user/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.wss.webservicestudy.web.user.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Api(tags = "유저")
 @RestController
 public class UserController {
+    @ApiOperation(value="초기테스트")
     @RequestMapping("/")
     public String home() {
         return "hello World";
