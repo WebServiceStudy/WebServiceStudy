bdb0835ee97d09d42c3fb2d9e59514f3337d3526
tkdgus97 <breakking@naver.com>
2022-12-02 21:58:53 +0900

    pull main and cherry pick

--- src/main/java/com/wss/webservicestudy/web/common/enums/Auth.java
+++
@@ -1,5 +0,0 @@
-package com.wss.webservicestudy.web.common.enums;
-
-public enum Auth {
-    USER, ADMIN
-}
--- src/main/java/com/wss/webservicestudy/web/common/enums/LoginType.java
+++
@@ -1,5 +0,0 @@
-package com.wss.webservicestudy.web.common.enums;
-
-public enum LoginType {
-    KAKAO, GOOGLE, NAVER
-}
