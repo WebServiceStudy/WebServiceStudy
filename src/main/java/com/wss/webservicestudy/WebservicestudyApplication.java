a8fa5b8aeae11ca07d38e09fd0d5980a37d37e26
je-pa <jieun0502@gmail.com>
2022-12-18 21:51:03 +0900

    Modify: WebservicestudyApplication - @CreatedDate를 위해 추가

--- src/main/java/com/wss/webservicestudy/WebservicestudyApplication.java
+++ src/main/java/com/wss/webservicestudy/WebservicestudyApplication.java
@@ -3,11 +3,13 @@ package com.wss.webservicestudy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WebservicestudyApplication {
 //	@Bean
 //	public PasswordEncoder passwordEncoder(){
