006931d621b5a35f1d302db164406075a0e86d0b
je-pa <jieun0502@gmail.com>
2022-12-18 21:53:33 +0900

    Modify: FeedMeet 주석 추가

--- src/main/java/com/wss/webservicestudy/web/feed/entity/FeedMeet.java
+++ src/main/java/com/wss/webservicestudy/web/feed/entity/FeedMeet.java
@@ -8,16 +8,18 @@ import javax.persistence.*;
 @Getter
 @Entity
 public class FeedMeet {
-
+    // 식별값
     @Id
     @Column(name = "MEET_ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    // 피드
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "FEED_ID")
     private Feed feed;
 
+    // 참여자
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ID")
     private User user;
