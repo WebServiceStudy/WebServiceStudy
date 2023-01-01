a4b46eacdb3627b164ac01e1d1ac25013b89fc52
je-pa <jieun0502@gmail.com>
2023-01-02 00:21:45 +0900

    Modify: WebservicestudyApplication - import 정리

--- src/main/java/com/wss/webservicestudy/WebservicestudyApplication.java
+++ src/main/java/com/wss/webservicestudy/WebservicestudyApplication.java
@@ -2,7 +2,7 @@ package com.wss.webservicestudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
