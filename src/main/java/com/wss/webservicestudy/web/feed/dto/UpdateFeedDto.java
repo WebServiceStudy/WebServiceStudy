eba04ba84364ff1ab9822a1c3be5f13df1607a7c
je-pa <jieun0502@gmail.com>
2023-01-02 00:16:17 +0900

    Modify: UpdateFeedDto - date 설정바꿈, 빌더추가

--- src/main/java/com/wss/webservicestudy/web/feed/dto/UpdateFeedDto.java
+++ src/main/java/com/wss/webservicestudy/web/feed/dto/UpdateFeedDto.java
@@ -1,23 +1,43 @@
 package com.wss.webservicestudy.web.feed.dto;
 
 import com.wss.webservicestudy.web.user.entity.User;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @ToString
 public class UpdateFeedDto {
+    @Builder
+    public UpdateFeedDto(String title, String content, LocalDateTime date, String addr, String latitude, String longitude, int maxUser, int minAge, int maxAge) {
+        this.title = title;
+        this.content = content;
+        this.date = date;
+        this.addr = addr;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.maxUser = maxUser;
+        this.minAge = minAge;
+        this.maxAge = maxAge;
+    }
+
     @NotBlank(message = "제목은 필수 입력값입니다.")
     private String title;
 
     // 내용
     @NotBlank(message = "내용은 필수 입력값입니다.")
     private String content;
+
+    // 모집일
+    @NotNull(message = "모집일은 필수 입력값입니다.")
+    private LocalDateTime date;
 
     // 모집 장소
     @NotBlank(message = "주소는 필수 입력값입니다.")
