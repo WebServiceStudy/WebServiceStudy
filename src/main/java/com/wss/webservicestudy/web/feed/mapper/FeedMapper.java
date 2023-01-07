7be14f58b54a10b901a32fa8d172c2b6cbf60668
je-pa <jieun0502@gmail.com>
2023-01-07 18:26:37 +0900

    Feat: FeedMapper, FeedMapperTest - mapstuct 사용, test

---
+++ src/main/java/com/wss/webservicestudy/web/feed/mapper/FeedMapper.java
@@ -0,0 +1,13 @@
+package com.wss.webservicestudy.web.feed.mapper;
+
+import com.wss.webservicestudy.web.feed.dto.CreateFeedDto;
+import com.wss.webservicestudy.web.feed.entity.Feed;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface FeedMapper {
+    FeedMapper INSTANCE = Mappers.getMapper(FeedMapper.class);
+
+    Feed toFeed(CreateFeedDto createFeedDto);
+}
