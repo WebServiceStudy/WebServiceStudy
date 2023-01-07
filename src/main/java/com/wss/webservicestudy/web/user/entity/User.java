f3f25477f9c9814e7184e2d71e9ab67bbc3f6d3c
je-pa <jieun0502@gmail.com>
2023-01-07 17:44:15 +0900

    Modify: User - feedMeet랑 양방향관계

--- src/main/java/com/wss/webservicestudy/web/user/entity/User.java
+++ src/main/java/com/wss/webservicestudy/web/user/entity/User.java
@@ -1,10 +1,13 @@
 package com.wss.webservicestudy.web.user.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.wss.webservicestudy.web.feed.entity.Feed;
+import com.wss.webservicestudy.web.feed.entity.FeedMeet;
 import com.wss.webservicestudy.web.user.type.LoginType;
 import com.wss.webservicestudy.web.user.type.Role;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -14,6 +17,7 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,8 +39,12 @@ public class User {
 
     private String birthday;
 
+//    @JsonManagedReference //참조가 되는 앞부분을 의미하며, 정상적으로 직렬화를 수행한다.
     @OneToMany(mappedBy = "writer")
     private List<Feed> feeds = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<FeedMeet> feedMeets = new ArrayList<>();
 
     @Builder
     public User(Long id, String name, String email, LoginType loginType, Role role) {
@@ -46,8 +54,4 @@ public class User {
         this.loginType = loginType;
         this.role = role;
     }
-
-    public User() {}
-
-
 }
