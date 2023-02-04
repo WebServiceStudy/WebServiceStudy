eb2c8cfb5ebf91a742fc78b6e556915bda019b7e
tkdgus97 <breakking@naver.com>
2023-02-04 21:22:10 +0900

    merge commit

--- src/main/java/com/wss/webservicestudy/web/user/controller/UserController.java
+++ src/main/java/com/wss/webservicestudy/web/user/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.wss.webservicestudy.web.user.controller;
 
 import com.wss.webservicestudy.web.common.ApiResponse;
+import com.wss.webservicestudy.web.common.security.jwt.dto.TokenInfo;
+import com.wss.webservicestudy.web.common.security.jwt.dto.TokenRequestDto;
 import com.wss.webservicestudy.web.common.util.SecurityUtil;
+import com.wss.webservicestudy.web.user.dto.UserLoginReqDto;
 import com.wss.webservicestudy.web.user.dto.UserRespDto;
 import com.wss.webservicestudy.web.user.service.UserService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,32 +24,8 @@ public class UserController {
         this.userService = userService;
     }
 
-<<<<<<< HEAD
-    @ApiOperation(value = "페이지 테스트", notes = "유저 페이지 테스트를 하기위한 내용을 적용한다.")
-    @RequestMapping("/user")
-    public String home() {
-        return "hello World";
-    }
-
-    @ApiOperation(value = "회원 가입", notes = "email, password 기반으로 회원가입")
-    @PostMapping("signup")
-    public ApiResponse<UserRespDto> signup(@RequestBody UserLoginReqDto reqDto) {
-        return ApiResponse.ok(userService.signup(reqDto));
-    }
-    @ApiOperation(value = "로그인", notes = "email, password를 통한 로그인")
-    @PostMapping("login")
-    public ApiResponse<TokenInfo> login(@RequestBody UserLoginReqDto reqDto) {
-        return ApiResponse.ok(userService.login(reqDto));
-    }
-
-    @ApiOperation(value = "jwt 토큰 재발급", notes = "토큰 재발급")
-    @PostMapping("reissue")
-    public ApiResponse<TokenInfo> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
-        return ApiResponse.ok(userService.reissue(tokenRequestDto));
-=======
     @GetMapping("/info")
     public ApiResponse<UserRespDto> getUserInfo() {
         return ApiResponse.ok(userService.findUserByEmail(SecurityUtil.getCurrentMember()));
->>>>>>> 3455069083ced3e77c241b0d551ba8a5d65eca1b
     }
 }
