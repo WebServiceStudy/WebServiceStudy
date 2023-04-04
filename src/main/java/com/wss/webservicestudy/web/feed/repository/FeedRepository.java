a971b958df14104a08129706015d6994afa4b899
gkfgran5037 <42172353+gkfgran5037@users.noreply.github.com>
2023-04-04 17:40:58 +0900

    refactor) Feed 조회 시 feedMeets의 user를 여러 번 조회하는 N+1 문제 해결 - 패치 조인

--- src/main/java/com/wss/webservicestudy/web/feed/repository/FeedRepository.java
+++ src/main/java/com/wss/webservicestudy/web/feed/repository/FeedRepository.java
@@ -3,6 +3,8 @@ package com.wss.webservicestudy.web.feed.repository;
 import com.wss.webservicestudy.web.feed.entity.Feed;
 import com.wss.webservicestudy.web.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,4 +19,10 @@ public interface FeedRepository extends JpaRepository<Feed, Long> {
     List<Feed> findAllByFeedMeets_User(User user);
 
     Feed findTop1ByOrderByIdDesc();
+
+    @Query("SELECT f FROM Feed f" +
+            " INNER JOIN FETCH f.feedMeets fm" +
+            " INNER JOIN FETCH fm.user" +
+            " WHERE f.id = :feedId")
+    Feed findByIdWithFeedMeets(@Param("feedId") long feedId);
 }
