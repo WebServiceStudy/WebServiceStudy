605ec2f74943798806de00a298299e408b19926f
gkfgran5037 <42172353+gkfgran5037@users.noreply.github.com>
2023-04-08 20:57:44 +0900

    feat) 조회 항목에 조회수, 카테고리, 본문 추가

--- src/main/java/com/wss/webservicestudy/web/feed/dto/FeedsRespDto.java
+++ src/main/java/com/wss/webservicestudy/web/feed/dto/FeedsRespDto.java
@@ -1,5 +1,6 @@
 package com.wss.webservicestudy.web.feed.dto;
 
+import com.wss.webservicestudy.web.category.entity.Category;
 import com.wss.webservicestudy.web.feed.entity.Feed;
 import com.wss.webservicestudy.web.feed.type.FeedStatus;
 import lombok.Getter;
@@ -17,7 +18,11 @@ public class FeedsRespDto {
 
     private String writerName;
 
+    private Category category;
+
     private String title;
+
+    private String content;
 
     private FeedStatus status;
 
@@ -35,15 +40,20 @@ public class FeedsRespDto {
 
     private int curFemale;
 
+    private int views;
+
     public FeedsRespDto(Feed feed) {
         this.id = feed.getId();
         this.writerName = feed.getWriterName();
+        this.category = feed.getCategory();
         this.title = feed.getTitle();
+        this.content = feed.getContent();
         this.status = feed.getStatus();
         this.date = feed.getDate();
         this.addr = feed.getAddr();
         this.maxUser = feed.getMaxUser();
         this.minAge = feed.getMinAge();
         this.maxAge = feed.getMaxAge();
+        this.views = feed.getViews();
     }
 }
